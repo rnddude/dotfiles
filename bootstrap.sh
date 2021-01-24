593b45ef81d9877c5b31377d0656e20b0d874717
rnddude <rnddude@icloud.com>
2021-01-24 10:01:39 +0100

    Continue moving to zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,17 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-declare -a FILES_TO_SYMLINK1=(
+declare -a FILES_TO_SYMLINK=(
 
     "zshrc"
+    "zsh_profile"
+    "zsh_aliases"
+    "zsh_options"
+    "zsh_prompt"
+    "zsh_function"
+    "zsh_exports"
+    "zsh_path"
+    "zsh_windowtitle"
     "inputrc"
     "curlrc"
     "gitattributes"
@@ -16,46 +24,13 @@ declare -a FILES_TO_SYMLINK1=(
     "editorconfig"
 )
 
-declare -a FILES_TO_SYMLINK2=(
-
-    "zsh_aliases"
-    "zsh_path"
-)
-
 i=""
 sourceFile=""
 targetFile=""
 
-for i in "${FILES_TO_SYMLINK1[@]}"; do
+for i in "${FILES_TO_SYMLINK[@]}"; do
 	sourceFile="$(pwd)/$i"
 	targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
-
-	if [ ! -e "$targetFile" ]; then
-
-	    execute \
-	        "ln -fs $sourceFile $targetFile" \
-	        "$targetFile → $sourceFile"
-
-	elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
-	    print_success "$targetFile → $sourceFile \n"
-	else
-		ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
-		if answer_is_yes; then
-
-		    rm -rf "$targetFile"
-
-		    execute \
-		        "ln -fs $sourceFile $targetFile" \
-		        "$targetFile → $sourceFile"
-		else
-		    print_error "$targetFile → $sourceFile \n"
-		fi
-	fi
-done
-
-for i in "${FILES_TO_SYMLINK2[@]}"; do
-	sourceFile="$(pwd)/$i"
-	targetFile="$HOME/.oh-my-zsh/custom/$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g").zsh"
 
 	if [ ! -e "$targetFile" ]; then
 
